3698cadda2ac664fee58702c8c3ee070cc4fe93c
Dania <daniajibellis@hotmail.com>
2021-11-22 21:03:49 -0500

    Displaying the image grids with Recycler

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -37,8 +37,8 @@ class OverviewViewModel : ViewModel() {
     val status: LiveData<String> = _status
 
     //agregamos una propiedad LivData para almacenas los objetos recibidos
-    private val _photos = MutableLiveData<MarsPhoto>()
-    val photos: LiveData<MarsPhoto> = _photos
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
 
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
@@ -55,8 +55,8 @@ class OverviewViewModel : ViewModel() {
         //iniciamos corrutina para realizar llamada a red retrofit en segundo plano
         viewModelScope.launch {
             try {
-                _photos.value = MarsApi.retrofitService.getPhotos()[0]
-                _status.value = "First Mars Image URL: ${_photos.value!!.imgSrcUrl}"
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = "Succes: Mars properties retrieved"
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
             }
